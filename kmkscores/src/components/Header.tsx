aa8d64cb3274b2449d0fecaf2f04ea0955333791
kyljmk <k.mansonkullin@googlemail.com>
2023-01-30 21:50:02 +0100

    Refactor Header

--- kmkscores/src/components/Header.tsx
+++ kmkscores/src/components/Header.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "../styles/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  fa1,
+  faBars,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <div className="header">
+      <FontAwesomeIcon className="header-menuIcon" icon={faBars} size="2xl" />
       <h1 className="header-title">kmkScores</h1>
+      <FontAwesomeIcon
+        className="header-searchIcon"
+        icon={faMagnifyingGlass}
+        size="xl"
+      />
     </div>
   );
 }
